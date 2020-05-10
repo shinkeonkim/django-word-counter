4d1b9649767232394cd1c56b8703f695ea1a469e
shinkeonkim <singun11@naver.com>
2020-05-10 16:13:38 +0900

    result.html 디자인 적용, 1차 완성

--- likelion8th/wordcount/views.py
+++ likelion8th/wordcount/views.py
@@ -14,10 +14,25 @@ def result(request):
     text = request.GET['fulltext']
     words = text.split()
     word_dictionary = {}
+    
     for word in words:
         try:
             word_dictionary[word]+=1
         except:
             word_dictionary[word]=1
     
-    return render(request, 'result.html', {'text':text,'word_dictionary': word_dictionary.items()})
+    word_dictionary = sorted(word_dictionary.items(), key = lambda t: (-t[1],t[0]))
+
+    word_count_dictionary = {}
+
+    for i in word_dictionary:
+        try:
+            word_count_dictionary[i[1]].append(i[0])
+        except:
+            word_count_dictionary[i[1]] = [i[0]]
+
+    
+    word_count_dictionary = [[i,sorted(j)] for i,j in word_count_dictionary.items()]
+    
+    
+    return render(request, 'result.html', {'text':text,'word_count_dictionary': word_count_dictionary})
